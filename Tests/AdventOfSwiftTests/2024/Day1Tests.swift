9a9016154b6e2226d3f4e3633d202a0b279cfcaa
Ian V Koeppe <ianvkoeppe@icloud.com>
2024-11-04 19:53:23 -0600

    Use Swift Tests

--- Tests/AdventOfSwiftTests/2024/Day1Tests.swift
+++ Tests/AdventOfSwiftTests/2024/Day1Tests.swift
@@ -1,28 +1,20 @@
-import XCTest
+import Testing
 @testable import AdventOfSwift
 
-class Day1Tests: XCTestCase {
-  func testPartOneExample() async {
-    measure {
-      XCTAssertEqual(Day1.partOne(lines: Files.readExample(year: 2024, day: 1)), 1)
-    }
+class Day1Tests {
+  @Test func testPartOneExample() async {
+    #expect(Day1.partOne(lines: Files.readExample(year: 2024, day: 1)) == 1)
   }
   
-  func testPartOne() async {
-    measure {
-      XCTAssertEqual(Day1.partOne(lines: Files.readProblem(year: 2024, day: 1)), 1)
-    }
+  @Test func testPartOne() async {
+    #expect(Day1.partOne(lines: Files.readProblem(year: 2024, day: 1)) == 1)
   }
   
-  func testPartTwoExample() async {
-    measure {
-      XCTAssertEqual(Day1.partTwo(lines: Files.readExample(year: 2024, day: 1)), 1)
-    }
+  @Test func testPartTwoExample() async {
+    #expect(Day1.partTwo(lines: Files.readExample(year: 2024, day: 1)) == 1)
   }
   
-  func testPartTwo() async {
-    measure {
-      XCTAssertEqual(Day1.partTwo(lines: Files.readProblem(year: 2024, day: 1)), 1)
-    }
+  @Test func testPartTwo() async {
+    #expect(Day1.partTwo(lines: Files.readProblem(year: 2024, day: 1)) == 1)
   }
 }
